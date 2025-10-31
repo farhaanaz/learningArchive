4bb7025e4891bed4b73d43a06502b76e05cf48d3
farhaanaz <130947553+farhaanaz@users.noreply.github.com>
2025-10-31 20:33:49 +0800

    Replace secret key values with placeholders
    Updated key_name and public_key values to placeholders.

--- terraform/keyPair.tf
+++ terraform/keyPair.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "dove-key" {
-  key_name   = "its a secret LOL"
-  public_key = "its a secret LOL"
+  key_name   = "your_key_name"
+  public_key = "your_public_key"
   # The public key above is for demonstration purposes only. In a real scenario, use your own public key.
   # To generate a new SSH key pair, you can use the ssh-keygen command and then cat the public key file to get the public key string.
 }
